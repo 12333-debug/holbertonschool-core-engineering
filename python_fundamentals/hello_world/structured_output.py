06f3e799a6fae760f27c47b44d595c816311c7ff
lonie <12333@holbertonstudents.com>
2026-05-04 16:19:34 +0200

    Create an executable file named structured_output.py that prints exactly:

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+Pi_approx = 3.1415
+Language = "Python"
+Version = 3
+my_guess = 1==1
+
+print("Language:",Language)
+print("Version:",Version)
+print("Pi approx: %.2f"% Pi_approx)
+print("Computation valid:",my_guess)

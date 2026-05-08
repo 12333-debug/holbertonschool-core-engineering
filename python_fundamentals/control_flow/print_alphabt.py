56cb7cb82a0d27137469b291856159cff4a585d9
lonie <12333@holbertonstudents.com>
2026-05-08 16:20:42 +0200

    Print the lowercase alphabet except for q and e

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-i=0
 
-for i in range(ord('a'),ord('z')+1):
-   
-    if(i != ord('e') and i != ord('q')):
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('e') and i != ord('q'):
         print(chr(i), end="")
-    i = i + 1
-	

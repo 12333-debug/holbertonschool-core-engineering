349a48fd257a9ffd2bd78412072cd748dc247d20
lonie <12333@holbertonstudents.com>
2026-05-08 22:46:00 +0200

    Write a function that prints a matrix of integers.

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, value in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d}".format(value), end=" ")
+            else:
+                print("{:d}".format(value))

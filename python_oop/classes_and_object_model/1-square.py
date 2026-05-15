7c4fea37a1f09c75da50f6b0f7bc938444db58ce
lonie <12333@holbertonstudents.com>
2026-05-15 14:48:10 +0200

    Define a class Square with:

--- python_oop/classes_and_object_model/1-square.py
+++ python_oop/classes_and_object_model/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Defines a Square class with a private size attribute."""
+
 class Square:
+    """Represents a square with a private size."""
     def __init__(self, size):
         self.__size = size
